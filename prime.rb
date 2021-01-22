21b7f51b6bf3c8ec7ddf762ea550eb65bf241374
Ahmaud R. Templeton <ahmaud@gmail.com>
2021-01-22 15:43:57 -0500

    commit tree d02c104fe046c015e4d88fe8eb9c20929d1052cc

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
+require 'pry'
 # Add  code here!
+
+def prime?(number)
+    return false if number < 2
+    return true if number == 2
+    return false if !number.integer?
+    (2..number-1).each { |num| return false if number % num == 0 }
+    true
+end
+
+
+# def prime?(number)
+#     if !number.integer?
+#         false
+#     elsif number == 2
+#         true
+#     elsif number < 2
+#         false
+#     elsif (2..number-1).each do |num| 
+#         number % num == 0
+#             true
+#         end
+#     end
+#         true
+# end
+
